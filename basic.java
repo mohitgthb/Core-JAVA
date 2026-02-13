9f7974027f0b6d239204dd15c869505a73648e17
Mohit Choudhari <mohitchoudhari162@gmail.com>
2026-02-13 15:00:56 +0530

    Exaplanation

--- basic.java
+++ basic.java
@@ -14,3 +14,27 @@ class basic {
         obj.Drive();
     }
 }
+
+
+// When JVM loads this class:
+
+// Class metadata → stored in Method Area
+
+// Static variables → stored in Method Area
+
+// Methods → stored in Method Area
+
+// 🔹 Object
+
+// An object is an instance created in Heap Memory.
+
+// Car c1 = new Car();
+
+
+// Memory breakdown:
+
+// c1 → stored in Stack (reference)
+
+// Actual object → stored in Heap
+
+// Object variables → stored inside Heap object
